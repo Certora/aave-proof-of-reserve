7ea801ec347968dbbd781ad0f99de97609173a3e
Michael M <MichaelM@certora.com>
2022-12-25 17:45:38 +0200

    adding recursive submodule

--- certora/scripts/runExecutorV2.sh
+++ certora/scripts/runExecutorV2.sh
@@ -4,7 +4,7 @@ certoraRun certora/harness/ProofOfReserveExecutorV2Harness.sol certora/harness/P
     --optimistic_loop \
     --loop_iter 3 \
     --solc solc8.16 \
-    --staging abakst/static-array-memcopy-loops \
+    --cloud \
     --packages solidity-utils=lib/solidity-utils/src chainlink-brownie-contracts=lib/chainlink-brownie-contracts/contracts/src/v0.8/ aave-address-book=lib/aave-address-book/src/ forge-std=lib/forge-std/src/ \
     --msg "ProofOfReserveExecutorV2"
     
