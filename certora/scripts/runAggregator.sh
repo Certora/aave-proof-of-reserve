1c1dd3f37e2fb4f80c8c769ce8840a4b2d9e4b5c
Michael M <MichaelM@certora.com>
2023-01-02 14:06:16 +0200

    resolving version error

--- certora/scripts/runAggregator.sh
+++ certora/scripts/runAggregator.sh
@@ -2,7 +2,7 @@ certoraRun certora/harness/ProofOfReserveAggregatorHarness.sol \
     --verify ProofOfReserveAggregatorHarness:certora/specs/aggregator.spec \
     --optimistic_loop \
     --loop_iter 3 \
-    --cloud \
+    --cloud master \
     --solc solc8.16 \
     --packages solidity-utils=lib/solidity-utils/src chainlink-brownie-contracts=lib/chainlink-brownie-contracts/contracts/src/v0.8/ \
     --msg "ProofOfReserveAggregator"
--- certora/scripts/runExecutorV3.sh
+++ certora/scripts/runExecutorV3.sh
@@ -5,7 +5,7 @@ certoraRun certora/harness/ProofOfReserveExecutorV3Harness.sol certora/harness/P
     --optimistic_loop \
     --loop_iter 3 \
     --solc solc8.16 \
-    --cloud \
+    --cloud master \
     --packages solidity-utils=lib/solidity-utils/src chainlink-brownie-contracts=lib/chainlink-brownie-contracts/contracts/src/v0.8/ aave-address-book=lib/aave-address-book/src/ forge-std=lib/forge-std/src/ \
     --msg "ProofOfReserveExecutorV3"
     
